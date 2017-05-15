6ee81e29ee86fa10fbe034b43dd5f6c571504336
Martin Mosegaard Amdisen <martin.amdisen@praqma.com>
2017-05-15 20:38:58 +0200

    Fix rubocop offenses

--- lib/fogbugz/commands/config.rb
+++ lib/fogbugz/commands/config.rb
@@ -12,23 +12,23 @@ module Fogbugz
         @file = File.join(Dir.home, PROPS)
         abort error_message unless File.exist?(file)
 
-        config = YAML.load_file(file) or abort error_message
+        config = YAML.load_file(file) || abort(error_message)
 
-        @email = config['email'] or abort error_message
-        @server = config['server'] or abort error_message
+        @email = config['email'] || abort(error_message)
+        @server = config['server'] || abort(error_message)
       end
 
       private
 
       def error_message
-        %{
+        %(
         Incomplete configuration file #{@file}
 
         Create the file with this content:
 
         email: you@company.com
         server: company.fogbugz.com
-      }
+      )
       end
     end
   end
